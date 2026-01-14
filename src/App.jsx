0ebb996b7849c030360d070b054ab58efa922ce1
Evgenii <7zhhovalyg@mail.ru>
2026-01-14 20:46:07 +0300

    Add character logic, basic app component, compress images

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,65 @@
-import './styles/App.css'
+import "./styles/App.css";
+import StartPage from "./pages/StartPage";
+import GamePage from "./pages/GamePage";
+import LoadingPage from "./pages/LoadingPage";
+import { useState } from "react";
+
+const LEVELS = {
+  easy: 4,
+  medium: 7,
+  hard: 10,
+};
 
 function App() {
+  const [pageId, setPageId] = useState("startPage");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isMusicPlaying, setIsMusicPlaying] = useState(false);
+  const [isSoundPlaying, setIsSoundPlaying] = useState(true);
+  const [level, setLevel] = useState("easy");
+
+  const runGamePage = () => {
+    setIsLoading(true);
+    setPageId("gamePage");
+  };
+
+  const runStartPage = () => {
+    setPageId("startPage");
+  };
+
+
+
+  return (
+    <>
+      {isLoading ? (
+        <LoadingPage />
+      ) : (
+        <>
+          {(pageId === "startPage" && (
+            <StartPage
+              configs={configs}
+              setLevel={setLevel}
+              runGamePage={runGamePage}
+            />
+          )) ||
+            (pageId === "gamePage" && (
+              <GamePage
+                configs={configs}
+                numberOfCards={LEVELS[level]}
+                setIsLoading={setIsLoading}
+                runStartPage={runStartPage}
+                key={crypto.randomUUID()}
+              />
+            ))}
+          <Footer
+            isMusicPlaying={isMusicPlaying}
+            setIsMusicPlaying={setIsMusicPlaying}
+            isSoundPlaying={isSoundPlaying}
+            setIsSoundPlaying={setIsSoundPlaying}
+          />
+        </>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/characters.js
@@ -0,0 +1,75 @@
+import LUFFY from "./assets/characters/LUFFY.png";
+import ZORO from "./assets/characters/ZORO.png";
+import NAMI from "./assets/characters/NAMI.png";
+import USOPP from "./assets/characters/USOPP.png";
+import SANJI from "./assets/characters/SANJI.png";
+import CHOPPER from "./assets/characters/CHOPPER.png";
+import ROBIN from "./assets/characters/ROBIN.png";
+import FRANKY from "./assets/characters/FRANKY.png";
+import BROOK from "./assets/characters/BROOK.png";
+import JINBEI from "./assets/characters/JINBEI.png";
+
+const characters = [
+  {
+    id: crypto.randomUUID(),
+    name: "Luffy",
+    src: LUFFY,
+    clicked: false,
+  },
+  {
+    id: crypto.randomUUID()(),
+    name: "Zoro",
+    src: ZORO,
+    clicked: false,
+  },
+  {
+    id: crypto.randomUUID()(),
+    name: "Nami",
+    src: NAMI,
+    clicked: false,
+  },
+  {
+    id: crypto.randomUUID()(),
+    name: "Usopp",
+    src: USOPP,
+    clicked: false,
+  },
+  {
+    id: crypto.randomUUID()(),
+    name: "Sanji",
+    src: SANJI,
+    clicked: false,
+  },
+  {
+    id: crypto.randomUUID()(),
+    name: "Chopper",
+    src: CHOPPER,
+    clicked: false,
+  },
+  {
+    id: crypto.randomUUID()(),
+    name: "Robin",
+    src: ROBIN,
+    clicked: false,
+  },
+  {
+    id: crypto.randomUUID()(),
+    name: "Franky",
+    src: FRANKY,
+    clicked: false,
+  },
+  {
+    id: crypto.randomUUID()(),
+    name: "Brook",
+    src: BROOK,
+    clicked: false,
+  },
+  {
+    id: crypto.randomUUID()(),
+    name: "Jinbei",
+    src: JINBEI,
+    clicked: false,
+  },
+];
+
+export default characters;
