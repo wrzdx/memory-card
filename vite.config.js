6c846e73da1a4983fef6a03e661f617434426ca3
Evgenii <7zhhovalyg@mail.ru>
2026-01-16 16:06:34 +0300

    Deployment commit

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/Memory-Card/',
-  plugins: [react(), svgr()],
-});
