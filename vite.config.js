b5d58f406cf2bc8a8c5aa420440b86ecf373e60b
Evgenii <7zhhovalyg@mail.ru>
2026-01-16 15:31:49 +0300

    Add readme and change base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/Memory-Card/',
+  base: '/Memory-Card/',
   plugins: [react(), svgr()],
 });
